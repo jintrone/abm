dbd8a717fd3283166d765c5251271afb15466e59
yuyangliang <mydoraemons@hotmail.com>
2015-11-25 14:24:27 -0500

    Update model_alpha_1.py

--- model_alpha_1.py
+++ model_alpha_1.py
@@ -3,7 +3,7 @@ import random
 
 class Post:
 	def __init__(self):
-		self.time = int(time.time())
+		self.time = time.time()
 		self.isb = random.random()
 		self.esb = random.random()
 		self.csb = random.random()
@@ -12,7 +12,7 @@ class Post:
 	def delete(self):
 		self.show = 0
 	def new(self, mui, mue, muc, sigma):
-		self.time = int(time.time())
+		self.time = time.time()
 		self.isb = random.gauss(mui, sigma)
 		self.esb = random.gauss(mue, sigma)
 		self.csb = random.gauss(muc, sigma)
@@ -21,16 +21,29 @@ class Post:
 
 class Agent:
 	def __init__(self):
-		self.isn = random.uniform(0.5,1)
-		self.esn = random.uniform(0.3, 0.8)
-		self.csn = random.uniform(0, 0.5)
+		self.isn = random.random()
+		self.esn = random.random()
+		self.csn = random.random()
 		self.tot = self.isn + self.esn + self.csn
 		self.stay = 1
 		self.post = 0
 		self.totsp = 0
 	def leave(self):
 		self.stay = 0
-	def post(self):
+	def addpost(self):
 		self.post = 1
 	def tally(self, recsp):
 		self.totsp += recsp
+		
+post = []		
+for i in range(0,10): post.append(Post())
+agent = Agent()
+for i in range(0,10): agent.tally(post[i].tot)
+if agent.totsp < 12.50: 
+	agent.leave()
+elif 12.50 <= agent.totsp < 17.50: 
+	pass
+else:
+	agent.post()
+	newpost = Post()
+	post.append(newpost.new(agent.isn, agent.esn, agent.csn, 0.05))
