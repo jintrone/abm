2f2083d575f98030bb66b3de45324b6e4dea7816
yuyangliang <mydoraemons@hotmail.com>
2015-12-08 17:04:10 -0500

    Update model_alpha_2.py

--- model_alpha_2.py
+++ model_alpha_2.py
@@ -1,5 +1,6 @@
 import time
 import random
+import uuid
 
 class Post:
 	def __init__(self):
@@ -36,16 +37,28 @@ class Agent:
 	def new(self):
 
 class Agent:
-	def __init__(self):
+	def __init__(self, lambd):
 		self.ten = random.expovariate(lambd)
 		self.stage = 0
-		if self.ten > 1/lambd: self.stage = 1
+		if self.ten > 1.0/lambd: self.stage = 1
 		self.know = random.random()
 		self.act = random.random()
 		self.inter = {}
+		self.id = uuid.uuid4().hex[:10]
+		self.stay = 1
+	def addinter(self, id):
+		self.inter[id] = 1
+	def updinter(self, id):
+		self.inter[id] += 1
+	def leave(self):
+		self.stay = 0		
 		
 class Post:
 	def __init__(self):
 		self.type = random.randint(0,1) #0 Question
 		self.iran = random.random()
 		self.auth = 'null'
+	def add(self, type, range, author):
+		self.type = type
+		self.iran = range
+		self.auth = author
