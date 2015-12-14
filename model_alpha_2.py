26de2ef12056c6d09a8f200612c8571f602d0709
yuyangliang <mydoraemons@hotmail.com>
2015-12-14 11:31:31 -0500

    new

--- model_alpha_2.py
+++ model_alpha_2.py
@@ -3,77 +3,119 @@ import random
 import uuid
 import math
 
+lambd = 0.01
+
 class Forum:
 	def __init__(self, num):
 		self.plist = []
 		self.alist = []
 		for i in range(0, num):
-			a = Agent(100)
-			p = Post(0, a)
+			a = Agent()
+			p = Post(1, a)
 			self.alist.append(a)
 			if a.stage == 0: 
 				p.addq(a)
 			else :
 				p.addc(a)
 			self.plist.append(p)
-
+	def gena(self, num):
+		for i in range(0, num):
+			a = Agent()
+			self.alist.append(a)
+				
 class Agent:
-	def __init__(self, lambd):
-		self.ten = random.expovariate(lambd)
+	def __init__(self):
+		self.ten = round(random.expovariate(lambd))
+		self.ften = 0
 		self.stage = 0
 		self.ineed = random.random()
-		if self.ten > math.log(10)/lambd: 
+		if self.ten >= round(math.log(4)/lambd): 
 			self.stage = 1
 			self.ineed = 0
+		else:
+			pass
 		self.know = random.random()
 		self.act = random.random()
 		self.inter = {}
 		self.id = uuid.uuid4().hex[:10]
 		self.stay = 1
 		self.capa = random.random()
+		self.fit = random.random()
+		self.dfit = self.fit - self.fit**2
+		self.ppost = []
+		
 	def addinter(self, id):
 		self.inter[id] = 1
 	def updinter(self, id):
 		self.inter[id] += 1
+		
+	def evalai(self, post, time):
+		if self.know >= post.iran: 
+			self.fit = self.fit + self.dfit
+			prob = random.random()
+			if prob <= self.act:
+				p = Post(time, self)
+				p.adda(self)
+				return self.ppost.append(p)
+			else: 
+				pass
+		else:
+			self.fit = self.fit - self.dfit
+	def evalqi(self, time):
+		prob = random.random()
+		if prob <= self.act:
+			p = Post(time, self)
+			p.addq(self)
+			return self.ppost.append(p)
+		else:
+			pass
+	def evalc(self, post, time):
+		if post.auth in self.inter:
+			self.fit = self.fit + self.dfit
+			prob = random.random()
+			if prob <= self.act:
+				p = Post(time, self)
+				p.addc(self)
+				return self.ppost.append(p)
+			else:
+				pass
+		else:
+			self.fit = self.fit - self.dfit
+	def evalqc(self, time):
+		prob = random.random()
+		if prob <= self.act:
+			p = Post(time, self)
+			p.addc(self)
+			return self.ppost.append(p)
+		else:
+			pass
+			
 	def leave(self):
-		self.stay = 0		
+		if self.fit < 0.1:
+			self.stay = 0
+		else:
+			pass
+	def addten(self):
+		self.ten += 1
+		self.ften += 1
+		if self.ten >= round(math.log(4)/lambd): 
+			self.stage = 1
+			self.ineed = 0
+		
 		
 class Post:
 	def __init__(self, time, agent):
-		self.info = 0
 		self.iran = 0
-		self.comm = 0
 		self.auth = agent.id
+		self.info = 1
 		self.time = time
 	def addq(self, agent):
-		self.type = 'Q'
-		self.info = 1
+		self.type = 'QI'
 		self.iran = random.gauss(agent.ineed, 0.0005)
 	def adda(self, agent):
-		self.type = 'A'
-		self.info = 1
-		self.iran = random.gauss(agent.ineed, 0.0005)
+		self.type = 'AI'
+		self.iran = random.gauss(agent.know, 0.0005)
 	def addc(self, agent):
 		self.type = 'C'
-		self.comm = 1
-	
-def postq(agent, ifit, cfit):
-	agent.act
-	agent.stage
-	ifit
-	cfit
-	newq = Post()
-	return newq.add(type, range, agent.id)
-
-def postir(agent, post):
-	agent.act
-	overlap = abs(agent.know - post.iran)
-	newr = Post()
-	return newr.add(type, range, agent.id)
-	
-def postcr(agent, post):
-	agent.act
-	if post.auth in agent.inter: times = agent.inter[post.auth]
-	newr = Post()
-	return newr.add(type, range, agent.id)
+		self.info = 0
 	
